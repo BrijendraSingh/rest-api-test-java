f72c586870ce96b85c3f6a9725c270f1047bc482
biswajit <biswajit.pattanayak@gmail.com>
2018-07-14 15:20:31 +0530

    Biswajit & Preeti: ignore out/ files

---
+++ src/test/java/utilities/AppConfig.java
@@ -0,0 +1,24 @@
+package utilities;
+
+import java.io.*;
+import java.util.Properties;
+
+public class AppUtilities {
+
+    private static String CONFIG_FILE = "config.properties";
+
+    public String getProperty(String propName) throws IOException {
+
+        Properties properties = new Properties();
+        properties.load(new FileInputStream(this.getPropertiesFile()));
+
+        return properties.getProperty(propName);
+    }
+
+    public File getPropertiesFile() throws FileNotFoundException{
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(CONFIG_FILE).getFile());
+
+        return file;
+    }
+}
