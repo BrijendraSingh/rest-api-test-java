77709e6270d9941744ded699891888bdd562f747
biswajit <biswajit.pattanayak@gmail.com>
2018-07-24 10:56:33 +0530

    Biswajit & Preeti: test implementation

--- src/test/java/GitHubApiTests/ResponseBodyTest.java
+++ src/test/java/GitHubApiTests/ResponseBodyTest.java
@@ -38,17 +38,61 @@ public class ResponseBodyTest {
 
     @Test
     public void shouldContainIdInUsersEndpoint(){
-        assert false;
+        given()
+                .pathParam("login", USER_NAME).
+        when()
+                .get("/users/{login}").
+        then()
+                .assertThat()
+                .body("id", equalTo(9004987));
     }
 
     @Test
     public void shouldContainProjectNameInReposEndpoint(){
-        assert false;
+        Map<String, Object> expectedItem = new HashMap<>();
+        expectedItem.put("name", "rest-api-test-java");
+
+        Response response = given()
+                .pathParam("userName", USER_NAME).
+                        when()
+                .request("GET", "users/{userName}/repos").
+                        then()
+                .extract()
+                .response();
+
+        List<Map<String, String>> repoDetails = from(response.asString()).getList(".");
+        assertThat(repoDetails, hasItem(new CustomTypeSafeMatcher<Map<String,String>>("an entrySet that contains " + expectedItem.entrySet()) {
+            @Override
+            protected boolean matchesSafely(Map<String, String> o) {
+                return hasItems(expectedItem.entrySet().toArray()).matches(o.entrySet());
+            }
+        }));
+
+//        JsonPath jsonPath = new JsonPath(response.asString()).setRoot(".");
+//        System.out.println(jsonPath);
     }
 
     @Test
     public void shouldContainLoginNameInFollowersEndpoint(){
-        assert false;
+        Map<String, Object> expectedItem = new HashMap<>();
+        expectedItem.put("login", "nalinikanth");
+
+        Response response = given()
+                                .pathParam("userName", USER_NAME).
+                            when()
+                                .get("users/{userName}/followers").
+                            then()
+                                .extract()
+                                .response();
+
+        assertThat(response.getHeader("X-RateLimit-Limit"), is("60"));
+        assertThat(response.getContentType(), containsString("application/json"));
+
+        JsonPath jsonPath = new JsonPath(response.asString());
+        ArrayList<Map<String, Object>> jsonList = jsonPath.get(".");
+        assertThat(jsonList.get(0), hasKey("login"));
+        assertThat(jsonList.get(0).get("login"), is(expectedItem.get("login")));
+
     }
 
 }
